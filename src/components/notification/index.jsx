7fdacdcead89687b2fe21884cbc9bc4ca2468cb4
Mashchenko Oleksandra <dremka@hotmail.com>
2018-09-05 10:44:12 +0300

    loader for notification added

--- src/components/notification/index.jsx
+++ src/components/notification/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import Loader from 'react-loader-spinner';
 // selectors
 import { getRecipeInProgress } from '../../redux/selectors/recipes';
 // styles
@@ -9,8 +10,9 @@ import styles from './styles.css';
 const Notification = ({ recipeName }) => (
   <div>
     {(recipeName && (
-      <h2>
+      <h2 className={styles.h2}>
         <span className={styles.span}>{recipeName}</span> is cooking...
+        <Loader type="Hearts" color="#D32F2F" height={100} width={100} />
       </h2>
     )) || <h2>No one recipe is cooking now</h2>}
   </div>
